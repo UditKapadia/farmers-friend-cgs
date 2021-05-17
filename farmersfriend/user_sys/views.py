6740889287a5f7b5cd30006388dc3ee0b2888d8c
UditKapadia <kapadiaudit@gmail.com>
2021-05-17 13:52:26 +0530

    updated some files

--- farmersfriend/user_sys/views.py
+++ farmersfriend/user_sys/views.py
@@ -2,22 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
-    context={}
-    return render(request,"user_sys/index.html", context)
+    context = {}
+    return render(request, "user_sys/index.html", context)
+
 
 def scientific_labs(request):
-    context={}
-    return render(request,"user_sys/scientific_lab",context)
+    context = {}
+    return render(request, "user_sys/scientific_lab.html", context)
+
 
 def market(request):
-    context={}
-    return render(request,"user_sys/market",context)
+    context = {}
+    return render(request, "user_sys/market.html", context)
+
 
 def weather_forecast(request):
-    context={}
-    return render(request,"user_sys/weather_forecast",context)
+    context = {}
+    return render(request, "user_sys/weather_forecast.html", context)
+
 
 def farm_ed(request):
-    context={}
-    return render(request,"user_sys/farm_ed",context)
+    context = {}
+    return render(request, "user_sys/farm_ed.html", context)
