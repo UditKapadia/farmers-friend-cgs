5d168c47b5aec8f24e20a8cc43ccd70e736d98ed
UditKapadia <kapadiaudit@gmail.com>
2021-05-17 13:42:17 +0530

    test

--- farmersfriend/farmersfriend/settings.py
+++ farmersfriend/farmersfriend/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'farmersfriend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', ],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
