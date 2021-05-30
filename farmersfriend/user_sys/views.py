d7a7e832960f10cae91b03b68d9fdab41783a2e5
uditk <55614597+UditKapadia@users.noreply.github.com>
2021-05-30 23:17:16 +0530

    Changed some form data

--- farmersfriend/user_sys/views.py
+++ farmersfriend/user_sys/views.py
@@ -25,7 +25,6 @@ def market(request):
 
 def weather_forecast(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=2ebe62ad7ad9ab7bf60a9e5bf054180e'
-    
 
     if request.method == 'POST':
         form = WeatherForm(request.POST)
@@ -35,16 +34,16 @@ def weather_forecast(request):
 
     city = Weather.objects.last()
 
-    r=requests.get(url.format(city)).json()
+    r = requests.get(url.format(city)).json()
     print(r)
     city_weather = {
         'city': city,
-        'temperature' : r['main']['temp'],
-        'description' : r['weather'][0]['description'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
     }
     # print(city_weather)
     context = {
-        'city_weather' : city_weather,
+        'city_weather': city_weather,
     }
     return render(request, "user_sys/weather_forecast.html", context)
 
@@ -68,13 +67,16 @@ def register(request):
     context = {}
     return render(request, "user_sys/register.html", context)
 
+
 def irrigation(request):
     context = {}
     return render(request, "user_sys/irrigation.html", context)
 
+
 def newtech(request):
     context = {}
     return render(request, "user_sys/newtech.html", context)
+
 
 def queryform(request):
     if request.method == "POST":
@@ -84,21 +86,25 @@ def queryform(request):
         des = request.POST.get('des')
         queryform = QueryForm(phone=phone, email=email, query=query, des=des)
         queryform.save()
-        messages.success(request, 'Your form has submitted successfully!')
-    
+        messages.success(request, 'Your form has been submitted successfully!')
+
     return render(request, "user_sys/queryform.html")
+
 
 def videobar(request):
     context = {}
     return render(request, "user_sys/videobar.html", context)
 
+
 def product(request):
     context = {}
     return render(request, "user_sys/product.html", context)
 
+
 def team(request):
     context = {}
     return render(request, "user_sys/team.html", context)
+
 
 def contact(request):
     context = {}
